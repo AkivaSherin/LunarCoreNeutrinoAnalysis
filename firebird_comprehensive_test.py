2c1179dba8cc55928b494d72ebd9e0f513ceff6e
AkivaSherin <Akivas@icloud.com>
2025-10-16 13:20:08 -0500

    added more firebird tests

---
+++ firebird_comprehensive_test.py
@@ -0,0 +1,22 @@
+import importlib
+lunar_neutrino_lib = importlib.import_module("lunar_neutrino_lib")
+importlib.reload(lunar_neutrino_lib)
+from lunar_neutrino_lib import *
+
+ratios = [3, 7, 30]
+radii = [100, 250, 400]
+for ratio in ratios:
+    for radius in radii:
+        make_heatmap_confidence_interval_plot(ratio, radius, 10)
+        print("made a heatmap 10 neutrinos")
+
+for ratio in ratios:
+    for radius in radii:
+        make_heatmap_confidence_interval_plot(ratio, radius, 100)
+        print("made a heatmap 100 neutrinos")
+
+for ratio in ratios:
+    for radius in radii:
+        make_heatmap_confidence_interval_plot(ratio, radius, 10000)
+        print("made a heatmap 10000 neutrinos")
+
--- firebird_custom_contours_test.py
+++ firebird_custom_contours_test.py
@@ -3,14 +3,14 @@ lunar_neutrino_lib = importlib.import_module("lunar_neutrino_lib")
 importlib.reload(lunar_neutrino_lib)
 from lunar_neutrino_lib import *
 
-ratios = [5, 10, 30]
-radii = [100, 400]
+ratios = [3, 7, 30]
+radii = [100, 250, 400]
 for ratio in ratios:
     for radius in radii:
-        make_heatmap_confidence_interval_plot(ratio, radius, 10, percentiles=(66, 90, 99))
-        print("made a heatmap 100 neutrinos")
+        make_heatmap_confidence_interval_plot(ratio, radius, 1000, percentiles=(30, 50, 90))
+        print("made a heatmap 1000 neutrinos")
 
 for ratio in ratios:
     for radius in radii:
-        make_heatmap_confidence_interval_plot(ratio, radius, 10, percentiles=(10, 60, 99.5))
-        print("made a heatmap 100 neutrinos")
+        make_heatmap_confidence_interval_plot(ratio, radius, 1000, percentiles=(20, 60, 95))
+        print("made a heatmap 1000 neutrinos")
