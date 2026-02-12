d59c4820a8baea868a3943f3f33f826eadffd54a
AkivaSherin <Akivas@icloud.com>
2026-02-12 12:27:38 -0500

    modified scatter test (more scatters)

--- firebird_test_scatter_ideal.py
+++ firebird_test_scatter_ideal.py
@@ -2,15 +2,15 @@ import importlib
 lunar_neutrino_lib = importlib.import_module("lunar_neutrino_lib")
 importlib.reload(lunar_neutrino_lib)
 from lunar_neutrino_lib import *
-ratios = [10, 14,]
+ratios = [10, 14]
 radii = [400, 550]
 
 for ratio in ratios:
     for radius in radii:
-        make_scatter_plot(ratio, radius, 1000, 100)
+        make_scatter_plot(ratio, radius, 1000, 1000)
         print("Made scatter plot 1000 neutrinos")
 
 for ratio in ratios:
     for radius in radii:
-        make_scatter_plot(ratio, radius, 10000, 100)
+        make_scatter_plot(ratio, radius, 10000, 1000)
         print("Made scatter plot 10000 neutrinos")
