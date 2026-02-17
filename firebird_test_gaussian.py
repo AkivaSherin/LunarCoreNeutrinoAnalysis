3ce567a548f0494962db8dad108db775c2bc502e
AkivaSherin <Akivas@icloud.com>
2026-02-17 12:02:51 -0500

    added gaussian pdf code (in lunar_neutrino_lib_copy.py) to test heatmaps. also a new idea about how to fix my code!

---
+++ firebird_test_gaussian.py
@@ -0,0 +1,21 @@
+import importlib
+lunar_neutrino_lib = importlib.import_module("lunar_neutrino_lib_copy")
+importlib.reload(lunar_neutrino_lib)
+from lunar_neutrino_lib import *
+ratios = [-5, 0, 5]
+radii = [0, 10, 100]
+
+for ratio in ratios:
+    for radius in radii:
+        make_heatmap_confidence_interval_plot(ratio, radius, 10)
+        print("made a heatmap 10 neutrinos")
+
+for ratio in ratios:
+    for radius in radii:
+        make_heatmap_confidence_interval_plot(ratio, radius, 100)
+        print("made a heatmap 100 neutrinos")
+
+for ratio in ratios:
+    for radius in radii:
+        make_heatmap_confidence_interval_plot(ratio, radius, 1000)
+        print("made a heatmap 10000 neutrinos")
