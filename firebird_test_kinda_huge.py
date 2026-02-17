dbe0874242985179e41ad3e3f3e63df3eeea089d
AkivaSherin <Akivas@icloud.com>
2026-02-17 12:17:41 -0500

    changed a test

--- firebird_test_kinda_huge.py
+++ firebird_test_kinda_huge.py
@@ -3,8 +3,8 @@ lunar_neutrino_lib = importlib.import_module("lunar_neutrino_lib")
 importlib.reload(lunar_neutrino_lib)
 from lunar_neutrino_lib import *
 
-ratios = [3, 5, 10, 30]
-radii = [100, 400]
+ratios = [5, 10]
+radii = [250, 500]
 for ratio in ratios:
     for radius in radii:
         make_heatmap_confidence_interval_plot(ratio, radius, 1000)
