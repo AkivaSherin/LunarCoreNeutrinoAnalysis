7e1a311f237efdbd4d808b5b8db72277ea45ff1f
AkivaSherin <Akivas@icloud.com>
2026-03-17 19:57:21 -0400

    could it be that i finally fixed my gaussian? turns out importlib doesn't work like I thought

--- firebird_test_gaussian.py
+++ firebird_test_gaussian.py
@@ -1,7 +1,7 @@
 import importlib
-lunar_neutrino_lib = importlib.import_module("lunar_neutrino_lib_copy")
-importlib.reload(lunar_neutrino_lib)
-from lunar_neutrino_lib import *
+lunar_neutrino_lib_copy = importlib.import_module("lunar_neutrino_lib_copy")
+importlib.reload(lunar_neutrino_lib_copy)
+from lunar_neutrino_lib_copy import *
 ratios = [-5, 0, 5]
 radii = [0, 10, 100]
 
@@ -9,12 +9,12 @@ for ratio in ratios:
     for radius in radii:
         make_heatmap_confidence_interval_plot(ratio, radius, 10)
         print("made a heatmap 10 neutrinos")
-
+"""
 for ratio in ratios:
     for radius in radii:
         make_heatmap_confidence_interval_plot(ratio, radius, 100)
         print("made a heatmap 100 neutrinos")
-
+"""
 """for ratio in ratios:
     for radius in radii:
         make_heatmap_confidence_interval_plot(ratio, radius, 1000)
