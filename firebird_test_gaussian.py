d4365ceeda872a1f07460f6b796238a3946941c4
AkivaSherin <Akivas@icloud.com>
2026-04-18 15:12:06 -0400

    changed test slightly

--- firebird_test_gaussian.py
+++ firebird_test_gaussian.py
@@ -12,6 +12,12 @@ for ratio in ratios:
     for radius in radii:
         make_heatmap_confidence_interval_plot(ratio, radius, 10)
         print("made a heatmap 10 neutrinos")
+
+
+for ratio in ratios:
+    for radius in radii:
+        make_heatmap_confidence_interval_plot(ratio, radius, 10)
+        print("made a heatmap 10 neutrinos")
 """
 for ratio in ratios:
     for radius in radii:
